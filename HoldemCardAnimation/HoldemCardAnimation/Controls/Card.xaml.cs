86d06cc607f9d0212f2fccfd2fc3af0e286064ba
Luke.Lee <kevin.lgs@aceenter.com>
2022-05-14 17:42:22 +0900

    초기 커밋.

---
+++ HoldemCardAnimation/HoldemCardAnimation/Controls/Card.xaml.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HoldemCardAnimation.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Card : Grid
+	{
+		public static readonly BindableProperty NumberProperty = BindableProperty.Create(nameof(Number), typeof(string), typeof(Card), null, BindingMode.TwoWay,  null);
+
+		public string Number
+		{
+			get => (string)GetValue(NumberProperty);
+			set => SetValue(NumberProperty, value);
+		}
+		public static readonly BindableProperty SuitProperty = BindableProperty.Create(nameof(Suit), typeof(string), typeof(Card), null, BindingMode.TwoWay, propertyChanged: SuitChanged);
+
+		private static void SuitChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var card = bindable as Card;
+
+			if ((string)newValue == "DIAMOND" ||
+			   (string)newValue == "HEART")
+			{
+				card.suit.Text = "◆";
+				card.number.TextColor = Color.Red;
+				card.suit.TextColor = Color.Red;
+			}
+			else
+			{
+				card.number.TextColor = Color.Black;
+				card.suit.TextColor = Color.Black;
+			}
+		}
+
+		public string Suit
+		{
+			get => (string)GetValue(SuitProperty);
+			set => SetValue(SuitProperty, value);
+		}
+		public Card()
+		{
+			InitializeComponent();
+		}
+		private int i = 0;
+		public void Drift()
+		{
+			if(i==0)
+			{
+				DriftUp();
+				i = 1;
+			}
+			else
+			{
+				DriftDown();
+				i = 0;
+			}
+		}
+		private void DriftUp()
+		{
+			front.RotateXTo(0, 250, Easing.Linear);
+		}
+		private void DriftDown()
+		{
+			front.RotateXTo(90, 250, Easing.Linear);
+		}
+	}
+}
---
+++ HoldemCardAnimation/HoldemCardAnimation/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HoldemCardAnimation
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+		{
+			//card1.RotateYTo(10, 250, Easing.CubicIn);
+		}
+
+		private void SwipeGestureRecognizer_Swiped_1(object sender, SwipedEventArgs e)
+		{
+
+		}
+
+		private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+		{
+			//card1.RotateXTo(20, 250, Easing.CubicIn);
+			//card1.RotateYTo(20, 250, Easing.CubicIn);
+			
+		}
+
+		private void TapGestureRecognizer_Tapped1(object sender, EventArgs e)
+		{
+			Card1.Drift();
+		}
+
+		private void TapGestureRecognizer_Tapped2(object sender, EventArgs e)
+		{
+			Card2.Drift();
+		}
+	}
+}
